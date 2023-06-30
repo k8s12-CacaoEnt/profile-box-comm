4e400ae5695f524b6c9a7b0e1a71d11506313d24
bakhuiwon326 <79272597+bakhuiwon326@users.noreply.github.com>
2023-06-30 14:32:50 +0900

    add SecurityConfig

--- src/main/java/com/goorm/profileboxcomm/config/SecurityConfig.java
+++ src/main/java/com/goorm/profileboxcomm/config/SecurityConfig.java
@@ -1,2 +1,42 @@
-package com.goorm.profileboxcomm.config;public class SecurityConfig {
+package com.goorm.profileboxcomm.config;
+
+
+import com.goorm.profileboxcomm.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CorsFilter;
+
+@EnableWebSecurity
+@Configuration
+@AllArgsConstructor
+public class SecurityConfig {
+
+    private CorsFilter corsFilter;
+    private AuthenticationConfiguration authenticationConfiguration;
+    private MemberRepository memberRepository;
+
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable(); // csrf
+        // 면접에서 물어볼만한 질문: 인증방식 중에 하나를 선택햇 ㅓ jwt를 선택한건데 왜 세션을 사용하냐?. 왜 jwt를 사용했는지?
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(corsFilter)
+                .addFilter(new JwtAuthenticationFilter(authenticationManager()))
+                .addFilter(new JwtAuthorizationFilter(authenticationManager(), memberRepository))
+                .formLogin().disable()
+                .httpBasic().disable()
+                .authorizeHttpRequests()
+                //.requestMatchers("api/v1/notice/admin/**").hasRole("ADMIN") 추후에 usertype을 나누는게 확실하다면 이 주석 풀기.
+                .anyRequest().permitAll();
+        return http.build();
+    }
+
+}
 }
